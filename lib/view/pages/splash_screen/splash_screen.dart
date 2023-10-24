7744c6e2b46f575d80812a2c2096211d573c7557
ashraf fathalla <ashrafffathalla>
2023-10-24 12:27:48 -0700

    commit and push setup

---
+++ lib/view/pages/splash_screen/splash_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import '../../../core/localization/check_local.dart';
+import '../../../core/size_config/size_config.dart';
+import '../../../data/local/hiva_helper.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>  with SingleTickerProviderStateMixin{
+  late AnimationController _animationController;
+  late Animation<double> _opacityAnimation;
+  // String? token = HiveHelper().getData("token");
+
+  late Widget startWidget;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+    _opacityAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(_animationController);
+
+    _animationController.forward();
+    // if (token != null) {
+    //   startWidget = const Home();
+    // } else {
+    //   startWidget = const AuthView();
+    // }
+    // Future.delayed(
+    //     const Duration(seconds: 5),
+    //     () => Navigator.pushReplacement(
+    //           context,
+    //           MaterialPageRoute(builder: (context) => startWidget),
+    //         ));
+  }
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Scaffold(
+        backgroundColor: const Color(0xff00B0B9),
+        body: Center(
+          child: Padding(
+            padding:  EdgeInsets.all(MediaQuery.of(context).size.width*0.08),
+            child: AnimatedBuilder(
+              animation: _opacityAnimation,
+              builder: (context, child) {
+                return Opacity(opacity: _opacityAnimation.value,
+                child: Image.asset(
+                  "assets/images/splash.png",
+                  gaplessPlayback: true,
+                ),
+                );
+              },
+            ),
+          ),
+        ));
+  }
+}
