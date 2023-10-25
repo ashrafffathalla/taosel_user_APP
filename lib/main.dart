29c06f51a1943c5fc7e8d8afe9d570e34cd20a8f
abudiyab-git2 <abudyiab.git2@gmail.com>
2023-10-25 10:04:52 +0300

    setup commit and push

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:taosel_user_app/core/localization/language_cubit.dart';
 import 'package:taosel_user_app/view/pages/splash_screen/splash_screen.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown],
+  );
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+    ),
+  );
   runApp(const MyApp());
 }
 
@@ -11,10 +26,35 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(),
-      home: const SplashScreen(),
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context,widget){
+        return BlocBuilder<LanguageCubit, Locale>(
+            builder:(_,locale){
+              return MaterialApp(
+                title: 'Taosel',
+                theme: ThemeData(),
+                localizationsDelegates: AppLocalizations.localizationsDelegates,
+                supportedLocales: AppLocalizations.supportedLocales,
+                localeResolutionCallback: (deviceLocale, supportedLocale) {
+                  if (deviceLocale != null) {
+                    for (Locale locale in supportedLocale) {
+                      if (deviceLocale.languageCode == locale.languageCode) {
+                        return deviceLocale;
+                      }
+                    }
+                  }
+                  return supportedLocale.first;
+                },
+                locale: locale,
+                home: const SplashScreen(),
+              );
+            }
+        );
+      },
+
     );
   }
 }
