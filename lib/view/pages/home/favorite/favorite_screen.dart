3ff0d414ced62e41bda3cb113bf1007d9ce0e00e
ashraf fathalla <ashrafffathalla>
2023-12-23 16:00:27 -0800

    some Edits

--- lib/view/pages/home/favorite/favorite_screen.dart
+++ lib/view/pages/home/favorite/favorite_screen.dart
@@ -10,78 +10,78 @@ class FavoriteScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var cubit = BlocProvider.of<FavouriteCubit>(context);
-    return BlocConsumer<FavouriteCubit,FavouriteState>(
-      listener: (context, state) {
-
-      },
-     builder: (context, state) {
-       return  Scaffold(
+    return Scaffold(
          appBar: AppBar(
            title: const Text("المفضله"),
          ),
-         body:state is GetFavouriteLoadingState?const Center(child: CircularProgressIndicator.adaptive()):SingleChildScrollView(
-           child: Column(
-             children: [
-             cubit.favouriteModel!.data==null?Center(
-               child: Column(
-                 children: [
-                   SizedBox(
-                     height: MediaQuery.of(context).size.height/3,
-                   ),
-                   Icon(Icons.notifications_off_outlined,size: 50.sp,),
-                   Text(
-                     'لا توجد مفضله حاليا',
-                     style: TextStyle(
-                       fontSize: 16.sp,
-                       fontWeight: FontWeight.w600,
-                     ),
-                   ),
-                 ],
-               ),
-             ):SizedBox(
-                 height: MediaQuery.of(context).size.height,
-                 child: ListView.separated(
-                     itemBuilder: (context, index) {
-                       return Container(
-                         height: 50.h,
-                         width: double.infinity,
-                         decoration: BoxDecoration(
-                           color: Colors.white,
-                           borderRadius: BorderRadius.circular(8),
-                         ),
-                         child: Row(
-                           children: [
-                             Icon(Icons.notifications_active_sharp,size: 40.sp,),
-                             Column(
-                               children: [
-                                 Text(cubit.favouriteModel!.data![index].name.toString(),style: TextStyle(
-                                   fontSize: 16.sp,
-                                   fontWeight: FontWeight.w600,
-                                 ),),
-                                 Text(cubit.favouriteModel!.data![index].price.toString(),style: TextStyle(
-                                     fontSize: 14.sp,
-                                     fontWeight: FontWeight.w600,
-                                     color: const Color(0XFF748BA0)
-                                 ),),
-                               ],
+         body:BlocConsumer<FavouriteCubit,FavouriteState>(
+             listener: (context, state) {
+               if(state is GetFavouriteLoadingState ){
+                 const CircularProgressIndicator.adaptive();
+               }
+             },
+             builder: (context, state) {
+               return state is GetFavouriteLoadingState?const Center(child: CircularProgressIndicator.adaptive()):SingleChildScrollView(
+                 child: Column(
+                   children: [
+                     cubit.favouriteModel!.data!.isEmpty?Center(
+                       child: Column(
+                         children: [
+                           SizedBox(
+                             height: MediaQuery.of(context).size.height/3,
+                           ),
+                           Icon(Icons.notifications_off_outlined,size: 50.sp,),
+                           Text(
+                             'لا توجد مفضله حاليا',
+                             style: TextStyle(
+                               fontSize: 16.sp,
+                               fontWeight: FontWeight.w600,
                              ),
-                           ],
-                         ),
-                       );
-                     },
-                     separatorBuilder: (context, index) {
-                       return SizedBox(
-                         height: 56.h,
-                       );
-                     },
-                     itemCount: 5),
-               ),
-             ],
-           ),
-         ),
-       );
-     },
-
-    );
+                           ),
+                         ],
+                       ),
+                     ):SizedBox(
+                       height: MediaQuery.of(context).size.height,
+                       child: ListView.separated(
+                           itemBuilder: (context, index) {
+                             return Container(
+                               height: 50.h,
+                               width: double.infinity,
+                               decoration: BoxDecoration(
+                                 color: Colors.white,
+                                 borderRadius: BorderRadius.circular(8),
+                               ),
+                               child: Row(
+                                 children: [
+                                   Icon(Icons.notifications_active_sharp,size: 40.sp,),
+                                   Column(
+                                     children: [
+                                       Text(cubit.favouriteModel!.data![index].name.toString(),style: TextStyle(
+                                         fontSize: 16.sp,
+                                         fontWeight: FontWeight.w600,
+                                       ),),
+                                       Text(cubit.favouriteModel!.data![index].price.toString(),style: TextStyle(
+                                           fontSize: 14.sp,
+                                           fontWeight: FontWeight.w600,
+                                           color: const Color(0XFF748BA0)
+                                       ),),
+                                     ],
+                                   ),
+                                 ],
+                               ),
+                             );
+                           },
+                           separatorBuilder: (context, index) {
+                             return SizedBox(
+                               height: 56.h,
+                             );
+                           },
+                           itemCount: 5),
+                     ),
+                   ],
+                 ),
+               );
+             },
+           ),);
   }
 }
